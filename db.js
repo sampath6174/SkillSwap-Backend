063ce9d373c6e00c6e57b0720831a6b534e2da77
sampath <sampath.dundi1234@gmail.com>
2026-09-09 15:06:26 +0530

    updated skillswap backend

--- db.js
+++ db.js
@@ -1,17 +1,23 @@
+require("dotenv").config();
+
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
     if (err) {
         console.log("Database connection failed:", err);
     } else {
-        console.log("MySQL connected successfully");
+        console.log("Aiven MySQL connected successfully");
     }
 });
 
